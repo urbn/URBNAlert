6e3b5937292a4083240bd29e6edf1e246d0b07c5
Ryan Garchinsky <ryang1428@gmail.com>
2015-03-04 08:37:56 -0500

    Initial commit

---
+++ Example/URBNAlert/URBNViewController.m
@@ -0,0 +1,29 @@
+//
+//  URBNViewController.m
+//  URBNAlert
+//
+//  Created by Ryan Garchinsky on 03/04/2015.
+//  Copyright (c) 2014 Ryan Garchinsky. All rights reserved.
+//
+
+#import "URBNViewController.h"
+
+@interface URBNViewController ()
+
+@end
+
+@implementation URBNViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
