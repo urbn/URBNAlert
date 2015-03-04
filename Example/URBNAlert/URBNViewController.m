aad40e4dfa3073dbe7ee1d9fdae6b47420fb1119
Ryan Garchinsky <ryang1428@gmail.com>
2015-03-04 15:45:24 -0500

    [FPMA-448] Styling & more hookup - Applying custom fonts & colors - Added a new example

--- Example/URBNAlert/URBNViewController.m
+++ Example/URBNAlert/URBNViewController.m
@@ -21,7 +21,6 @@
 
 - (IBAction)activeAlertTouch:(id)sender {
     URBNAlertController *alertContoller = [[URBNAlertController alloc] initActiveAlertWithTitle:@"My Alert Title that could be 2 lines but no more than 2" message:@"Message and message and message and going on forever and ever." hasInput:YES buttons:@[@"Yes"]];
-    alertContoller.touchOutsideToDismiss = NO;
     
     __weak typeof(URBNAlertController) *weakAlertController = alertContoller;
     [alertContoller setButtonTouchedBlock:^(URBNAlertController *alertController, NSInteger index) {
@@ -34,9 +33,25 @@
 
 - (IBAction)activeAlertTwoButtonTouch:(id)sender {
     URBNAlertController *alertContoller = [[URBNAlertController alloc] initActiveAlertWithTitle:@"My Alert Title" message:@"Message and message and message and going on forever and ever." hasInput:YES buttons:@[@"Yes", @"No"]];
+    
+    __weak typeof(URBNAlertController) *weakAlertController = alertContoller;
+    [alertContoller setButtonTouchedBlock:^(URBNAlertController *alertController, NSInteger index) {
+        NSLog(@"button touch at index: %ld", index);
+        [weakAlertController dismissAlert];
+    }];
+    
+    [alertContoller showAlert];
+}
+
+- (IBAction)activeAlertColoredTouch:(id)sender {
+    URBNAlertController *alertContoller = [[URBNAlertController alloc] initActiveAlertWithTitle:@"Colorful Alert" message:@"Message and message and message and going on forever and ever." hasInput:YES buttons:@[@"Yes", @"No"]];
     alertContoller.touchOutsideToDismiss = NO;
     alertContoller.buttonBackgroundColor = [UIColor redColor];
-    
+    alertContoller.buttonDenialBackgroundColor = [UIColor greenColor];
+    alertContoller.backgroundColor = [UIColor orangeColor];
+    alertContoller.titleColor = [UIColor purpleColor];
+    alertContoller.messageColor = [UIColor blueColor];
+
     __weak typeof(URBNAlertController) *weakAlertController = alertContoller;
     [alertContoller setButtonTouchedBlock:^(URBNAlertController *alertController, NSInteger index) {
         NSLog(@"button touch at index: %ld", index);
