1bef61339212d5bb5740d6a2519f1a659b00af5e
Ryan Garchinsky <ryang1428@gmail.com>
2015-04-13 12:56:18 -0400

    updated pods

--- Example/Pods/URBNConvenience/Pod/Classes/URBNMacros.h
+++ Example/Pods/URBNConvenience/Pod/Classes/URBNMacros.h
@@ -35,6 +35,7 @@
 #define SYSTEM_VERSION_LESS_THAN(v)                ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 
+#define IS_OS_8_OR_LATER     (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
 #define IS_OS_7_OR_LATER     (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0"))
 #define IS_OS_6_OR_LATER     (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"6.0"))
 #define IS_OS_5_1_OR_LATER   (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"5.1"))
