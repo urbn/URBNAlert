7eb4dd05fa78b5710fcd142197e3520801453ad4
Nicole Souvenir <nsouvenir@urbn.com>
2019-09-16 10:12:08 -0400

    added utf16 encoding

--- URBNAlert/Classes/AlertView.swift
+++ URBNAlert/Classes/AlertView.swift
@@ -209,12 +209,12 @@ extension AlertView: AlertButtonContainer {
 
 extension AlertView: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let charCount = textField.text?.count ?? 0
+        let charCount = textField.text?.utf16.count ?? 0
         if let charCount = textField.text?.count, range.length + range.location > charCount {
             return false
         }
         
-        let newLength = charCount + string.count - range.length
+        let newLength = charCount + string.utf16.count - range.length
         
         return newLength < configuration.styler.textField.maxLength
     }
